a4d26016dadea920bbeec9185848e40298ec9d9d
Bolade Akinniyi <bolade@Bolades-MacBook-Pro.local>
2024-08-25 15:39:12 +0100

    chore: dashboard stats

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
@@ -6,7 +6,21 @@ export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get('/stats')
-  async dashboardState() {
-    return await this.dashboardService.getStats();
+  async dashboardState(
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+  ) {
+    return await this.dashboardService.getStats(startDate, endDate);
+  }
+
+  @Get('/closed-records')
+  async getClosedReconciliations(
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+  ) {
+    return await this.dashboardService.getClosedReconciliations(
+      startDate,
+      endDate,
+    );
   }
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Account } from 'src/entities/account.entity';
 import { Reconciliation } from 'src/entities/reconciliation.entity';
-import { error } from 'src/utils/api-response.util';
-import { Repository } from 'typeorm';
+import { error, success } from 'src/utils/api-response.util';
+import { Repository, LessThan, MoreThan, Between } from 'typeorm';
+import * as moment from 'moment';
 
 @Injectable()
 export class DashboardService {
@@ -14,32 +15,115 @@ export class DashboardService {
     private reconciliationRepository: Repository<Reconciliation>,
   ) {}
 
-  async getStats() {
+  async getStats(startDate?: string, endDate?: string) {
     try {
+      const dateFilter =
+        startDate && endDate
+          ? {
+              updatedAt: Between(new Date(startDate), new Date(endDate)),
+              createdAt: Between(new Date(startDate), new Date(endDate)),
+            }
+          : {};
+
       const totalAccounts = await this.accountRepository.count();
       const totalReconciliations = await this.reconciliationRepository.count();
       const totalOpenedStageTwoReconciliations =
         await this.reconciliationRepository.count({
-          where: { approved_one: true, approved_two: false },
+          where: { approved_one: true, approved_two: false, ...dateFilter },
         });
       const totalOpenedReconciliation =
         await this.reconciliationRepository.count({
-          where: { approved_one: false, approved_two: false },
+          where: { approved_one: false, approved_two: false, ...dateFilter },
         });
       const totalClosedReconciliation =
         await this.reconciliationRepository.count({
-          where: { approved_one: true, approved_two: true },
+          where: { approved_one: true, approved_two: true, ...dateFilter },
+        });
+      const totalPartiallyUsedReconciliation =
+        await this.reconciliationRepository.count({
+          where: { balance: MoreThan(0), approved_one: false, ...dateFilter },
+        });
+      const totalOverdueReconciliation =
+        await this.reconciliationRepository.count({
+          where: {
+            approved_one: false,
+            updatedAt: LessThan(moment().subtract(14, 'days').toDate()),
+            ...dateFilter,
+          },
         });
 
+      const totalStageOneAmount = await this.reconciliationRepository
+        .createQueryBuilder('reconciliation')
+        .select(
+          'SUM(CASE WHEN reconciliation.approved_one = false AND reconciliation.amount_used IS NOT NULL THEN reconciliation.balance ELSE reconciliation.credit_amount END)',
+          'sum',
+        )
+        .where(dateFilter)
+        .getRawOne();
+
+      const totalStageTwoAmount = await this.reconciliationRepository
+        .createQueryBuilder('reconciliation')
+        .select('SUM(reconciliation.amount_used)', 'sum')
+        .where('reconciliation.approved_one = true')
+        .andWhere(dateFilter)
+        .getRawOne();
+
+      const totalFinalStageAmount = await this.reconciliationRepository
+        .createQueryBuilder('reconciliation')
+        .select('SUM(reconciliation.credit_amount)', 'sum')
+        .where(
+          'reconciliation.approved_one = true AND reconciliation.approved_two = true',
+        )
+        .andWhere(dateFilter)
+        .getRawOne();
+
+      const totalPartiallyUsedReconciliationAmount =
+        await this.reconciliationRepository
+          .createQueryBuilder('reconciliation')
+          .select('SUM(reconciliation.balance)', 'sum')
+          .where(
+            'reconciliation.balance > 0 AND reconciliation.approved_one = false',
+          )
+          .andWhere(dateFilter)
+          .getRawOne();
+
       return {
-        totalAccounts,
-        totalReconciliations,
-        totalOpenedStageTwoReconciliations,
-        totalOpenedReconciliation,
-        totalClosedReconciliation,
+        stats: {
+          totalAccounts,
+          totalReconciliations,
+          totalOpenedStageTwoReconciliations,
+          totalOpenedReconciliation,
+          totalClosedReconciliation,
+          totalPartiallyUsedReconciliation,
+          totalOverdueReconciliation,
+          totalStageOneAmount: totalStageOneAmount.sum || 0,
+          totalStageTwoAmount: totalStageTwoAmount.sum || 0,
+          totalFinalStageAmount: totalFinalStageAmount.sum || 0,
+          totalPartiallyUsedReconciliationAmount:
+            totalPartiallyUsedReconciliationAmount.sum || 0,
+        },
       };
     } catch (err) {
       return error('Failed to fetch stats', 500);
+    }
+  }
+
+  async getClosedReconciliations(startDate?: string, endDate?: string) {
+    try {
+      const dateFilter =
+        startDate && endDate
+          ? {
+              updatedAt: Between(new Date(startDate), new Date(endDate)),
+              createdAt: Between(new Date(startDate), new Date(endDate)),
+            }
+          : {};
+
+      const closedReconciliations = await this.reconciliationRepository.find({
+        where: { approved_one: true, approved_two: true, ...dateFilter },
+      });
+      return success(closedReconciliations, 'Records fetched successfully');
+    } catch (err) {
+      return error('Failed to fetch closed reconciliations', 500);
     }
   }
 }
